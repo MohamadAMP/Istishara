cd48d1146a51779184fa2b8e16f6cdcf59fd5521
Mohamad Sabra <mrs23@mail.aub.edu>
2021-04-22 22:24:25 +0300

    fixed functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,17 +6,17 @@ admin.initializeApp();
 const db = admin.firestore();
 const fcm = admin.messaging();
 
-export const sendToDevice = functions.firestore.document("ratings/{ratingId}").onCreate(async snapshot =>{
+export const sendToDevice = functions.firestore.document("messagePile/{messagePileId}").onCreate(async snapshot =>{
 
-    const rating  = snapshot.data();
-    const querySnapshot = await db.collection("rating").doc(rating.uid).collection("tokens").get();
+    const message  = snapshot.data();
+    const querySnapshot = await db.collection("mesagePile").doc(message.toUID).collection("tokens").get();
 
     const tokens = querySnapshot.docs.map(snap => snap.id);
 
     const payload: admin.messaging.MessagingPayload = {
         notification: {
-          title: "New Order!",
-          body: "you sold a ${rating.stars} for ${rating.order}",
+          title: "New Message From ${message.nameFrom}",
+          body: "${message.content}",
           click_action: "FLUTTER_NOTIFICATION_CLICK",
         }
       };
